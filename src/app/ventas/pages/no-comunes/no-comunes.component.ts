187c41b93dc4352452c8848f9d03c1824b4424cb
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-20 12:27:48 +0200

    ADD async, json pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -29,4 +30,33 @@ export class NoComunesComponent {
   borrarCliente() {
     this.clientes.pop();
   }
+
+  persona = {
+    nombre: 'Alejandro',
+    edad: 36,
+    direccion: 'Madrid, España',
+  };
+
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false,
+    },
+  ];
+
+  miObservable = interval(2000);
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 }
